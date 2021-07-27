8b2b6600229829794bc1e26272df34640890c251
anthonyshadow <anthony.shadowitz@gmail.com>
2021-07-26 23:33:38 -0400

    add a useState Hook

--- my-app/src/components/Accordion.js
+++ my-app/src/components/Accordion.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
+ 
+const Accordion = ({ items }) => {
 
-const Accordion = ({items}) => {
-    const renderedItems = items.map( item => {
-        return <React.Fragment key={item.title}>
-            <div className='title active'>
-               <i className='dropdown icon'></i>
-               {item.title}
-            </div>
+  const [ activeIndex, setActiveIndex]  = useState(null); 
 
-            <div className='content active'>
-                <p>{item.content}</p>
-            </div>
-        </React.Fragment>
-    })
+  const onTitleClick = (index) => {
+    setActiveIndex(index);
+  };
 
-    return <div className="ui styled accordion">
-        {renderedItems}
-    </div>
+  const renderedItems = items.map((item, index) => {
+    return (
+      <React.Fragment key={item.title}>
+        <div className="title active" onClick={() => onTitleClick(index)}>
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className="content active">
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
+
+  return <div className="ui styled accordion">
+      {renderedItems}
+      <h1>{activeIndex}</h1>
+      </div>;
 };
 
 export default Accordion;
