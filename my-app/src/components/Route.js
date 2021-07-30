eee62341051163ee733fa47d04887778a50552e7
anthonyshadow <anthony.shadowitz@gmail.com>
2021-07-30 11:29:01 -0400

    set up links so that they don't make a whole new batch of requests everytime they are clicked

--- my-app/src/components/Route.js
+++ my-app/src/components/Route.js
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
+
 
 const Route = ({path, children}) => {
 
-    return window.location.pathname === path ? children : null;
+    const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+    const onLocationChange = () => {
+        setCurrentPath(window.location.pathname)
+    };
+
+    useEffect(() => {
+        window.addEventListener('popstate', onLocationChange, {capture: true})
+
+        return () => {
+            window.removeEventListener('popstate', onLocationChange, {capture: true})
+        };
+    }, []);
+
+    return currentPath === path ? children : null;
 
 };
 
