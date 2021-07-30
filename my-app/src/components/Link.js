bdfd7f846221ea5194bee3e63d8cf515c519b256
anthonyshadow <anthony.shadowitz@gmail.com>
2021-07-30 11:34:16 -0400

    add handling command/ctrl clicks on links

--- my-app/src/components/Link.js
+++ my-app/src/components/Link.js
@@ -3,6 +3,13 @@ import React from 'react';
 const Link = ( {className, href, children}) => {
 
     const onClick = (event) => {
+        //metaKey is for MacOS, ctrlKey is for windows
+        //this detects if those keys are being held down while clicking the link
+        //and it will not run the rest of the function and allow the user to open the link in a new tab
+        if (event.metaKey || event.ctrlKey) {
+            return ;
+        }
+
         event.preventDefault();
         window.history.pushState({},'', href);
 
